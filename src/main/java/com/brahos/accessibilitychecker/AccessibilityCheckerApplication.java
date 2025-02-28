40cf82dff77559034fa7926b8d2f71c812f45fda
JustinAikyne <88371212+JustinAikyne@users.noreply.github.com>
2025-02-28 17:42:50 +0530

    main file changed

--- src/main/java/com/brahos/accessibilitychecker/AccessibilityCheckerApplication.java
+++ src/main/java/com/brahos/accessibilitychecker/AccessibilityCheckerApplication.java
@@ -2,14 +2,13 @@ package com.brahos.accessibilitychecker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.brahos.accessibilitychecker")
-public class AccessibilityCheckerApplication extends SpringBootServletInitializer {
+public class AccessibilityCheckerApplication {
 
 	public static void main(String[] args) {
 
